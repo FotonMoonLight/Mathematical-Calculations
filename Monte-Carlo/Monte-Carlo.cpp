6b972aaaeb22758b6d33f5861adea677381c66c5
FotonMoonLight <datahardofficial@gmail.com>
2022-10-05 17:41:00 +0300

    Monte-Carlo
    -Вычисление объема по высоте и радиусу методом Монте-Карло

---
+++ Monte-Carlo/Monte-Carlo.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	const double pi = 3.1415;
+	double R, H, V, V0, x, y, z;
+
+	int N = 100, n = 0;//Количевство внутренних точек в фигуре,повысить значение N для увеличения точности
+
+	cout << "Enter Radius= ";
+	cin >> R;
+	cout << "Enter Height= ";
+	cin >> H;
+	V0 = 4 * R * R * (H + R);//Объём паралепипеда
+	
+	for (int i = 0; i <= N; i++) 
+	{
+		x = 2 * i * R / N - R;
+		for (int j = 0; j <= N; j++) 
+		{
+			y = 2 * j * R / N - R;
+			for (int k = 0; k <= N; k++)
+			{
+				z = k * (H + R) / N;
+				if(((sqrt(x * x + y * y) / R <= z / H) && (z <= H)) || ((x * x + y * y + (z - H) * (z - H) <= R * R) && (z > H)))n++;
+			}
+		}
+	}
+	
+	V = V0 * n / pow(N + 1, 3);
+	cout << "Точное значение : Приближенное" << endl;
+	cout << "V = " << V << " : "<< pi*R*R*H/3+2*pi*pow(R,3)/3 << endl;
+	system("pause");
+	return 0;
+	
+}
